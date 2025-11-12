727a8c9f90ac0f8872f0a3322e5a4d601b5a4874
BABAR-TAHSEEN55 <babarsmoke24@gmail.com>
2025-11-12 23:25:55 +0530

    random

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
-import re
-
-import requests
-from bs4 import BeautifulSoup
-
-# Url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
-
-# result = requests.get(Url)
-# # print(result.text)
-# doc = BeautifulSoup(result.text, "html.parser")
-# title = doc.title
-# print(title.string)
-
-# print(res)
-# res = doc.find_all(string="51")
-
-
-# print(doc.prettify())
-
-with open("index.html", "r") as f:
-    doc = BeautifulSoup(f, "html.parser")
-    title = doc.title
-    # result = doc.find("option")
-    # resutl = doc.find_all(["p"], class_="yolo")
-    result = doc.find_all(string=re.compile("\$.*"), limit=1)
-
-print(result)
-for i in result:
-    print(i.strip())
-# result[""] = "new Value"
-# result["color"] = "blue"
+# import re
+#
+# import requests
+# from bs4 import BeautifulSoup
+#
+# # Url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+#
+# # result = requests.get(Url)
+# # # print(result.text)
+# # doc = BeautifulSoup(result.text, "html.parser")
+# # title = doc.title
+# # print(title.string)
+#
+# # print(res)
+# # res = doc.find_all(string="51")
+#
+#
+# # print(doc.prettify())
+#
+# with open("index.html", "r") as f:
+#     doc = BeautifulSoup(f, "html.parser")
+#     title = doc.title
+#     # result = doc.find("option")
+#     # resutl = doc.find_all(["p"], class_="yolo")
+#     result = doc.find_all(string=re.compile("\$.*"), limit=1)
+#
 # print(result)
-# print(title.string)
-# print(result.attrs)
+# for i in result:
+#     print(i.strip())
+# # result[""] = "new Value"
+# # result["color"] = "blue"
+# # print(result)
+# # print(title.string)
+# # print(result.attrs)
