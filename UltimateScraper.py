931bfe6ad4c9365dc1551902835dcfc7da8ecf06
BABAR-TAHSEEN55 <babarsmoke24@gmail.com>
2025-11-13 18:59:06 +0530

    feat: Sqlite implemented to store the scraped data

--- UltimateScraper.py
+++ UltimateScraper.py
@@ -1,7 +1,8 @@
 import os
 import random
 import sqlite3
-from sqlite3.dbapi2 import sqlite_version
+import time
+from urllib.parse import urljoin
 from urllib.robotparser import RobotFileParser
 
 import requests
@@ -12,13 +13,30 @@ from requests.adapters import HTTPAdapter, Retry
 load_dotenv()
 
 
+def Insert(quotes, cur, conn):
+    for quote_data in quotes:
+        tags_str = ", ".join(quote_data["tags"])
+        cur.execute(
+            "INSERT INTO Quotes (quote,author,tags) VALUES (?,?,?)",
+            (quote_data["quote"], quote_data["author"], tags_str),
+        )
+        # print(f"Inserted successfully")
+    print("Inserted Successfully")
+
+
+def GetData(cur):
+    cur.execute("SELECT * FROM Quotes")
+    data = cur.fetchall()
+    return data
+
+
 URL2 = os.getenv("URL2")
 ROBO = os.getenv("ROBO_URL")
 
 if not URL2:
     raise ValueError("URL2 Environmental Variable is required!")
 if not ROBO:
-    raise ValueError("URL2 Environmental Variable is required!")
+    raise ValueError("ROBO  Variable is required!")
 
 robo = RobotFileParser()
 robo.set_url(ROBO)
@@ -27,7 +45,12 @@ robo.read()
 if not robo.can_fetch("*", URL2):
     raise Exception("Scraping not Allowed")
 
-conn = sqlite3.connect("client_data.db")
+conn = sqlite3.connect("scrape.db")
+cur = conn.cursor()
+cur.execute(
+    "CREATE TABLE IF NOT EXISTS Quotes (quote VARCHAR,author VARCHAR , tags VARCHAR)"
+)
+conn.commit()
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
@@ -49,10 +72,10 @@ proxy = {"http": random.choice(proxies_list), "https": random.choice(proxies_lis
 
 
 try:
-    res = session.get(URL2, headers=headers, timeout=10)
-    res.raise_for_status()
-    print(res)
-    soup = BeautifulSoup(res.content, "lxml")
+    # res = session.get(URL2, headers=headers, timeout=10)
+    # res.raise_for_status()
+
+    # soup = BeautifulSoup(res.content, "lxml")
     # body = soup.body
     # # print(body)
     # href = body.find_all("a")
@@ -61,20 +84,47 @@ try:
     #     print(i)
     #     print()
     quotes = []
+    links = []
+    # quote_traverse = soup.select("ul.pager li.next a")
+    # quote_end = soup.select("ul.pager li.previous")
+    current_url = URL2
+    while True:
+        print(f"Scraping {current_url}")
+        res = session.get(current_url, headers=headers, timeout=10)
+        res.raise_for_status()
+        soup = BeautifulSoup(res.content, "lxml")
 
-    # Debug: Check if div.quote elements exist
-    quote_divs = soup.select("div.quote")
+        # Process quotes on current page
+        quote_divs = soup.select("div.quote")
 
-    for quotes_div in quote_divs:
-        quotest_text = quotes_div.select_one("span.text").get_text()
-        quotest_author = quotes_div.select_one("small.author").get_text()
-        quotest_tags = quotes_div.select("div.tags a.tag")
-        tags = [tag.get_text(strip=True) for tag in quotest_tags]
-        # print(tags)
-        # print(quotest_text)
-        # print(quotest_author)
+        for quotes_div in quote_divs:
+            quotest_text = quotes_div.select_one("span.text").get_text()
+            quotest_author = quotes_div.select_one("small.author").get_text()
+            quotest_tags = quotes_div.select("div.tags a.tag")
+            tags = [tag.get_text(strip=True) for tag in quotest_tags]
+            # print(tags)
+            # print(quotest_text)
+            # print(quotest_author)
 
-        quotes.append({"quote": quotest_text, "author": quotest_author, "tags": tags})
+            quotes.append(
+                {"quote": quotest_text, "author": quotest_author, "tags": tags}
+            )
+
+        # Check for next page
+        quote_traverse = soup.select("ul.pager li.next a")
+        if quote_traverse:
+            href = quote_traverse[0].get("href")
+            if href:
+                fullUrl = urljoin(URL2, href)
+                current_url = fullUrl
+                links.append(fullUrl)
+                current_url = fullUrl
+                time.sleep(3)
+                # print(href)
+            else:
+                break
+        else:
+            break
 
         # for span in spans:
         #     print(span.get_text())
@@ -89,10 +139,16 @@ try:
     for i, v in enumerate(quotes):
         print(v)
         print()
+
+    Insert(quotes, cur, conn)
+    conn.commit()
+
+    # result = GetData(cur)
+    # print(result)
+    # conn.close()
+    print(links)
+    print(len(quotes))
+
+
 except requests.exceptions.RequestException as e:
     print("Exception:", e)
-
-
-# Store the data into Sqlite
-# Traverse through the entire page
-# FAFO
